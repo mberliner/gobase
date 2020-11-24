5f2360aada0dde854afa0432f0e2bd593222e3cf
max <no@usar.com>
2020-11-23 22:27:15 -0300

    refactopr config

--- 10-servicios_rest/entities_service/repository/config.go
+++ 10-servicios_rest/entities_service/repository/config.go
@@ -16,6 +16,15 @@ var (
 )
 
 func init() {
+
+	initRepo()
+
+	UserRepo = NewUserRepository(db)
+	PersonaRepo = NewPersonaRepository(db)
+
+}
+
+func initRepo() {
 	var err error
 	db, err = sql.Open("mysql", "test:test987@tcp(localhost:3306)/go_test?charset=utf8&parseTime=true")
 	if err != nil {
@@ -28,10 +37,5 @@ func init() {
 		panic(err)
 	}
 	mysql.SetLogger(logger.GetLogger())
-
 	logger.Info("Conectado a Mysql")
-
-	UserRepo = NewUserRepository(db)
-	PersonaRepo = NewPersonaRepository(db)
-
 }
