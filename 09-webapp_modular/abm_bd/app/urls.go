b9a4227aec192d69eb1ea242544f696ad5214373
max <no@usar.com>
2022-09-20 21:20:16 -0300

    inicio modular

---
+++ 09-webapp_modular/abm_bd/app/urls.go
@@ -0,0 +1,20 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/mberliner/gobase/09-webapp_modular/abm_bd/controller"
+)
+
+func mapUrls() {
+	http.HandleFunc("/", controller.Index)
+	http.HandleFunc("/seccion", controller.Seccion)
+	http.HandleFunc("/usuario", controller.AltaUser)
+	http.HandleFunc("/login", controller.Login)
+	http.HandleFunc("/logout", controller.Logout)
+	http.HandleFunc("/persona/crear", controller.CrearPersona)
+	http.HandleFunc("/persona", controller.AbmPersona)
+	http.HandleFunc("/persona/borrar", controller.BorrarPersona)
+	http.HandleFunc("/persona/editar", controller.ActualizarPersona)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+}
